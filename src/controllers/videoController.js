dcaafd373469e82a03789ccc2af6aca068c0f0fa
HyeonWooGa <yeonwoopark22@gmail.com>
2022-04-06 16:13:41 +0900

    #4.7&8 URL Parameter

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,12 @@
 export const trending = (req, res) => res.send("Home Page Videos");
 export const search = (req, res) => res.send("Search Videos");
 
-export const see = (req, res) => res.send("Watch Videos");
-export const edit = (req, res) => res.send("Edit Videos");
+export const watch = (req, res) => {
+    return res.send(`Watch Video #${req.params.id}`);
+};
+export const edit = (req, res) => {
+    return res.send("Edit Videos");
+};
 export const deletVideo = (req, res) => res.send("Delete Videos");
 export const upload = (req, res) => res.send("Upload Videos");
 
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,13 +1,13 @@
 import express from "express";
-import { see, edit, deletVideo, upload } from "../controllers/videoController"
+import { watch, edit, deletVideo, upload } from "../controllers/videoController"
 
 
 const videoRouter = express.Router();
 
-videoRouter.get("/:id", see);
-videoRouter.get("/:id/edit", edit);
-videoRouter.get("/:id/delete", deletVideo);
 videoRouter.get("/upload", upload);
+videoRouter.get("/:id(\\d+)", watch);
+videoRouter.get("/:id(\\d+)/edit", edit);
+videoRouter.get("/:id(\\d+)/delete", deletVideo);
 
 
 export default videoRouter;
