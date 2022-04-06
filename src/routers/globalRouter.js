4a89cc3fe05e69d33e947b0200bca4b1379dac53
HyeonWooGa <yeonwoopark22@gmail.com>
2022-04-06 12:15:38 +0900

    #4.3 Exports

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
+import { trending } from "../controllers/videoController";
+import { join } from "../controllers/userController";
 
 
 const globalRouter = express.Router();
 
-const handleHome = (req, res) => res.send("Home");
-
-globalRouter.get("/", handleHome);
+globalRouter.get("/", trending);
+globalRouter.get("/join", join);
 
 
 export default globalRouter; // 누구든 globalRouter.js를 import하면 globalRoute 자체를 임포트하게 됌
