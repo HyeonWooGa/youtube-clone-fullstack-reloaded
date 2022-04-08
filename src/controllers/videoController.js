4b7bac4715aeb71437325a9660a58560c6fdd29a
HyeonWooGa <yeonwoopark22@gmail.com>
2022-04-09 06:10:51 +0900

    #5.4 Variables to Templates

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,8 @@
-export const trending = (req, res) => res.render("home");
+export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
 export const search = (req, res) => res.send("Search Videos");
 
-export const watch = (req, res) => res.render("watch");
-export const edit = (req, res) => res.render("edit")
+export const watch = (req, res) => res.render("watch", { pageTitle: "Watch" });
+export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" })
 export const deletVideo = (req, res) => res.send("Delete Videos");
 export const upload = (req, res) => res.send("Upload Videos");
 
