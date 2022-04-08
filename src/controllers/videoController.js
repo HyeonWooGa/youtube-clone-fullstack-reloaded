b8d311030b9cfcc2ebbbe46415e6833453ad90e8
HyeonWooGa <yeonwoopark22@gmail.com>
2022-04-09 05:25:36 +0900

    #5.3 Extending Templates

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,9 +2,7 @@ export const trending = (req, res) => res.render("home");
 export const search = (req, res) => res.send("Search Videos");
 
 export const watch = (req, res) => res.render("watch");
-export const edit = (req, res) => {
-    return res.send("Edit Videos");
-};
+export const edit = (req, res) => res.render("edit")
 export const deletVideo = (req, res) => res.send("Delete Videos");
 export const upload = (req, res) => res.send("Upload Videos");
 
