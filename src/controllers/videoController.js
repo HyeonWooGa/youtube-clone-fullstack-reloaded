62801b6397fadebd8cc653d0f7991eb81463d056
HyeonWooGa <yeonwoopark22@gmail.com>
2022-04-09 15:55:25 +0900

    #6.0 Array Database part One

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,40 @@
+let videos = [
+    {
+        title: "First Video",
+        rating: 5,
+        comments: 2,
+        createdAt: "2 minutes ago",
+        views: 59,
+        id: 1, 
+    },
+    {
+        title: "Second Video",
+        rating: 5,
+        comments: 2,
+        createdAt: "2 minutes ago",
+        views: 59,
+        id: 2, 
+    },
+    {
+        title: "Third Video",
+        rating: 5,
+        comments: 2,
+        createdAt: "2 minutes ago",
+        views: 59,
+        id: 3, 
+    },
+];
+
 export const trending = (req, res) => {
-    const videos = [];
     return res.render("home", { pageTitle: "Home", videos });
 }
 export const search = (req, res) => res.send("Search Videos");
 
-export const watch = (req, res) => res.render("watch", { pageTitle: "Watch" });
+export const watch = (req, res) => {
+    const { id } = req.params; //const id = req.params.id
+    const video = videos[id-1];
+    res.render("watch", { pageTitle: `Watching ${video.title}` });
+}
 export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" })
 export const deletVideo = (req, res) => res.send("Delete Videos");
 export const upload = (req, res) => res.send("Upload Videos");
