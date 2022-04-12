586bc7c4a1c751e7ab4b825d376768a72976bf51
HyeonWooGa <yeonwoopark22@gmail.com>
2022-04-12 17:09:47 +0900

    #6.11&12 Our First Query

---
+++ src/init.js
@@ -0,0 +1,14 @@
+import "./db";
+import "./models/Video";
+import app from "./server";
+
+const PORT = 4000;
+
+const handleListening = () => console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
+
+app.listen(PORT, handleListening);
+// Callback = 서버가 시작될 때 작동하는 함수
+// 서버에게 어떤 port를 listening 할 줄 알려줘야함
+// port는 컴퓨터의 문이나 창문과 같은 것
+// 클릭이벤트 바닐라 JS 와 비슷
+// localhost:4000 으로 방금 만든 서버에 접속 가능
--- src/server.js
+++ src/server.js
@@ -1,13 +1,9 @@
-import "./db";
-import "./models/Video";
 import express from "express";
 //const express = require("express");
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
-
-const PORT = 4000;
 
 const app = express();
 //express app 생성 ,server
@@ -81,15 +77,8 @@ const handleProtected = (req, res) => {
 /* app.use(logger); // global middleware, 순서를 조심해야 한다, 모든 route 에 사용됌 */
 /* app.use(privateMiddleware); */
 
+export default app;
 
-const handleListening = () => console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
-
-app.listen(PORT, handleListening);
-// Callback = 서버가 시작될 때 작동하는 함수
-// 서버에게 어떤 port를 listening 할 줄 알려줘야함
-// port는 컴퓨터의 문이나 창문과 같은 것
-// 클릭이벤트 바닐라 JS 와 비슷
-// localhost:4000 으로 방금 만든 서버에 접속 가능
 
 
 
