8ed7b8dc9de27471e9fdead51c1d4811f6461a55
HyeonWooGa <yeonwoopark22@gmail.com>
2022-04-25 19:29:56 +0900

    #6.27 Search part Two

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -59,8 +59,15 @@ export const deleteVideo = async (req, res) => {
     return res.redirect("/");
 }
 
-export const search = (req, res) => {
+export const search = async (req, res) => {
     const {keyword} = req.query;
-    console.log("should serch for ", keyword);
-    return res.render("search", { pageTitle: "Search" });
+    let videos = [];
+    if(keyword) {
+        videos = await Video.find({
+            title: {
+                $regex: new RegExp(keyword, "i"),
+            }
+        });
+    }
+    return res.render("search", { pageTitle: "Search", videos });
 }
