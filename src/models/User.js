d3f320ac15391f73ad6823c15c5f00f9237a373b
HyeonWooGa <yeonwoopark22@gmail.com>
2022-04-27 04:23:38 +0900

    #7.3 Form Validation

--- src/models/User.js
+++ src/models/User.js
@@ -1,3 +1,4 @@
+import bcrypt from "bcrypt";
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
@@ -6,7 +7,11 @@ const userSchema = new mongoose.Schema({
     password: { type: String, required: true },
     name: { type: String, required:true },
     location: String,
-})
+});
+
+userSchema.pre('save', async function() {
+    this.password = await bcrypt.hash(this.password, 5);
+});
 
 const User = mongoose.model("User", userSchema);
 export default User;
