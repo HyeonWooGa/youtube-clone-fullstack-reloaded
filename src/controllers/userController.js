bb7bf5c7e5395fa2ed8be839ac9c1c2343e05f3e
HyeonWooGa <yeonwoopark22@gmail.com>
2022-04-27 05:31:16 +0900

    #7.5 Login part One

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -17,16 +17,35 @@ export const postJoin = async (req, res) => {
             errorMessage: "This username/email is already taken.",
         });
     }
-    await User.create({
-        name,
-        username,
-        email,
-        password,
-        location,
-    });
+    try {
+        await User.create({
+            name,
+            username,
+            email,
+            password,
+            location,
+        });
+    } catch(error) {
+        return res.status(400).render("join", { pageTitle: "Join", errorMessage: error._message, });
+    }
     return res.redirect("/login");
 };
-export const login = (req, res) => res.send("Login");
+
+export const getLogin = (req, res) => res.render("login", { pageTitle: "Login"});
+export const postLogin = async (req,res) => {
+    const { username, pssword } = req.body;
+    const exists = await User.exists({ username });
+    if(!exists) {
+        return res
+          .status(400)
+          .render("login", { 
+            pageTitle: "Login", 
+            errorMessage: "An account with this username does not exists.",
+        });
+    }
+    // check if password correct
+    res.end();
+}
 
 export const logout = (req, res) => res.send("Log Out");
 export const edit = (req, res) => res.render("edit");
