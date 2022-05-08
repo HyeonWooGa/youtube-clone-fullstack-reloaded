e42164d58ef0971737b720380fd7e52c6128eda8
HyeonWooGa <yeonwoopark22@gmail.com>
2022-05-08 09:32:29 +0900

    #8.1,2,3,4

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -139,7 +139,24 @@ export const getEdit = (req, res) => {
     return res.render("edit-profile", { pageTitle: "Edit Profile"});
 };
 
-export const postEdit = (req, res) => {
-    return res.render("edit-profile");
+export const postEdit = async (req, res) => {
+    const { session: {user : {_id}}, body: { name, email, username, location} } = req; // ES6 const id = req.session.user.id & const { name, email, username, location } = req.body;
+    const updateUser = await User.findByIdAndUpdate(_id, {
+        name, email, username, location,
+    },
+    { new: true });
+    req.session.user = updateUser;
+    return res.redirect("/users/edit");
+};
+
+export const getChangePassword = (req, res) => {
+    if (req.session.user.socialOnly) return res.redirect("/");
+    return res.render("users/change-password", {pageTitle: "Change Password"});
+};
+
+export const postChangePassword = (req, res) => {
+    //
+    return res.redirect("/");
 }
+
 export const see = (req, res) => res.send("See");
--- src/middlewares.js
+++ src/middlewares.js
@@ -2,5 +2,22 @@ export const localsMiddleware = (req, res, next) => {
     res.locals.loggedIn = Boolean(req.session.loggedIn); // req.session.loggedIn 이 null, undefined 일 수 있으니 Boolean 으로 감싸줌
     res.locals.siteName = "Wetube";
     res.locals.loggedInUser = req.session.user || {}; // req.session.user 가 undefined 일 수 있으니 logeedInUser 는 loggedIn 상태에서만 사용 ex)if loggedIn 하위 항목으로
+    console.log(req.session.user)
     next();
-}
+};
+
+export const protectorMiddleware = (req, res, next) => {
+    if(req.session.loggedIn) {
+        return next();
+    } else {
+        return res.redirect("/login");
+    }
+};
+
+export const publicOnlyMiddleware = (req, res, next) => {
+    if(!req.session.loggedIn) {
+        return next();
+    } else {
+        return res.render("/");
+    }
+};
