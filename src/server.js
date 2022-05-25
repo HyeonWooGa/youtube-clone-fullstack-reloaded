d0ff977556e360ab284690c8912f875c3185bb94
HyeonWooGa <yeonwoopark22@gmail.com>
2022-05-25 17:50:39 +0900

    #12.0 Register View Controller
    Study about difference between 'views' & 'api views'

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
+import apiRouter from "./routers/apiRouter";
 import { localsMiddleware } from "./middlewares";
 
 const app = express();
@@ -73,6 +74,7 @@ app.use("/static", express.static("assets"));
 app.use("/", rootRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
+app.use("/api", apiRouter);
 
 /* const handleHome = (req, res) => {
     return res.send("I love middlewares");
