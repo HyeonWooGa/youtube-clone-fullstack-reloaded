3c72c8c8537295c3e5f052681e301a1958df41df
HyeonWooGa <yeonwoopark22@gmail.com>
2022-05-26 13:53:23 +0900

    #13.1 Video Preview

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,11 +1,13 @@
 const startBtn = document.getElementById("startBtn");
+const video = document.getElementById("preview")
 
 const handleStart = async () => {
     const stream = await navigator.mediaDevices.getUserMedia({
-        audio: true, 
-        video:true,
+        audio: false, 
+        video: { width: 600, height: 400},
     });
-    console.log(stream);
+    video.srcObject = stream;
+    video.play();
 };
 
 startBtn.addEventListener("click", handleStart);
