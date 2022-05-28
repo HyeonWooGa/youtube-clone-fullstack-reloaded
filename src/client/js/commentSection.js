d32bc777d702501655af03d8ac4672d2ba827732
HyeonWooGa <yeonwoopark22@gmail.com>
2022-05-28 14:22:24 +0900

    #16.7 Realtime Comments

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,13 +1,28 @@
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
 
+const addComment = (text) => {
+    const videoComments = document.querySelector(".video__comments ul");
+    const newComment = document.createElement("li");
+    newComment.className = "video__comment";
+    const icon = document.createElement("i");
+    icon.className = "fas fa-comment";
+    console.log("icon:", icon);
+    const span = document.createElement("span");
+    span.innerText = ` ${text}`;
+    console.log("span:", span);
+    newComment.appendChild(icon);
+    newComment.appendChild(span);
+    videoComments.prepend(newComment);
+};
+
 const handleSubmit = async (event) => {
     event.preventDefault();
     const textarea = form.querySelector("textarea");
     const text = textarea.value;
     const videoId = videoContainer.dataset.id;
     if (text === "") return;
-    await fetch(`/api/videos/${videoId}/comment`, {
+    const { status } = await fetch(`/api/videos/${videoId}/comment`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
@@ -15,7 +30,9 @@ const handleSubmit = async (event) => {
         body: JSON.stringify({ text }),
     });
     textarea.value = "";
-    window.location.reload();
+    if (status === 201) {
+        addComment(text);
+    }
 };
 
 const handleEnter = async (event) => {
@@ -26,7 +43,7 @@ const handleEnter = async (event) => {
         const text = textarea.value;
         const videoId = videoContainer.dataset.id;
         if (text === "") return;
-        await fetch(`/api/videos/${videoId}/comment`, {
+        const { status } = await fetch(`/api/videos/${videoId}/comment`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
@@ -34,7 +51,9 @@ const handleEnter = async (event) => {
             body: JSON.stringify({ text }),
         });
         textarea.value = "";
-        window.location.reload();
+        if (status === 201) {
+            addComment(text);
+        }
     }
 };
 
