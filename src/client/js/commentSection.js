cf6c3acaa9e81db53d4e78f0ec63ee4ed5c23178
HyeonWooGa <yeonwoopark22@gmail.com>
2022-06-03 23:52:26 +0900

    #16.9 Code Challenge

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,70 +1,90 @@
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
 
+let deleteComments = document.querySelectorAll("#delete__comment");
+
 const addComment = (text, id) => {
-    const videoComments = document.querySelector(".video__comments ul");
-    const newComment = document.createElement("li");
-    newComment.dataset.id = id;
-    newComment.className = "video__comment";
-    const icon = document.createElement("i");
-    icon.className = "fas fa-comment";
-    console.log("icon:", icon);
-    const span = document.createElement("span");
-    span.innerText = ` ${text}`;
-    const span2 = document.createElement("span");
-    span2.innerText = ` ❌`;
-    console.log("span:", span);
-    console.log("span2:", span2);
-    newComment.appendChild(icon);
-    newComment.appendChild(span);
-    newComment.appendChild(span2);
-    videoComments.prepend(newComment);
+  const videoComments = document.querySelector(".video__comments ul");
+  const newComment = document.createElement("li");
+  newComment.dataset.id = id;
+  newComment.className = "video__comment";
+  const icon = document.createElement("i");
+  icon.className = "fas fa-comment";
+  console.log("icon:", icon);
+  const span = document.createElement("span");
+  span.innerText = ` ${text} `;
+  const btnDelete = document.createElement("button");
+  btnDelete.innerText = `❌`;
+  btnDelete.id = "deleteComments";
+  console.log("span:", span);
+  console.log("btnDelete:", btnDelete);
+  newComment.appendChild(icon);
+  newComment.appendChild(span);
+  newComment.appendChild(btnDelete);
+  videoComments.prepend(newComment);
 };
 
 const handleSubmit = async (event) => {
+  event.preventDefault();
+  const textarea = form.querySelector("textarea");
+  const text = textarea.value;
+  const videoId = videoContainer.dataset.id;
+  if (text === "") return;
+  const response = await fetch(`/api/videos/${videoId}/comment`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ text }),
+  });
+  if (response.status === 201) {
+    textarea.value = "";
+    const { newCommentId } = await response.json();
+    addComment(text, newCommentId);
+  }
+};
+
+const handleEnter = async (event) => {
+  const { key } = event;
+  if (key === "Enter") {
     event.preventDefault();
     const textarea = form.querySelector("textarea");
     const text = textarea.value;
     const videoId = videoContainer.dataset.id;
     if (text === "") return;
     const response = await fetch(`/api/videos/${videoId}/comment`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ text }),
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ text }),
     });
     if (response.status === 201) {
-        textarea.value = "";
-        const { newCommentId } = await response.json();
-        addComment(text, newCommentId);
+      textarea.value = "";
+      const { newCommentId } = await response.json();
+      addComment(text, newCommentId);
     }
+  }
 };
 
-const handleEnter = async (event) => {
-    const { key } = event;
-    if (key === 'Enter') {
-        event.preventDefault();
-        const textarea = form.querySelector("textarea");
-        const text = textarea.value;
-        const videoId = videoContainer.dataset.id;
-        if (text === "") return;
-        const response = await fetch(`/api/videos/${videoId}/comment`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ text }),
-        });
-        if (response.status === 201) {
-            textarea.value = "";
-            const { newCommentId } = await response.json();
-            addComment(text, newCommentId);
-        }
-    }
+if (form) {
+  form.addEventListener("submit", handleSubmit);
+  window.addEventListener("keydown", handleEnter);
+}
+
+const handDeleteComment = async (event) => {
+  const li = event.srcElement.parentNode;
+  const {
+    dataset: { id: commentId },
+  } = li;
+  await fetch(`/api/comments/${commentId}/delete`, {
+    method: "DELETE",
+  });
+  li.remove();
 };
 
-if(form) {
-    form.addEventListener("submit", handleSubmit);
-    window.addEventListener("keydown", handleEnter);
+if (deleteComments) {
+  deleteComments.forEach((deleteComment) => {
+    deleteComment.addEventListener("click", handDeleteComment);
+  });
 }
