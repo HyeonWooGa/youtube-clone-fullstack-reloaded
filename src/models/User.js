147fc92deb5a300bddb779ffa51c9993510e6fd2
HyeonWooGa <yeonwoopark22@gmail.com>
2022-06-09 23:14:27 +0900

    v -> V

--- src/models/User.js
+++ src/models/User.js
@@ -2,21 +2,21 @@ import bcrypt from "bcrypt";
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    email: { type: String, required: true, unique: true },
-    avatarUrl: String,
-    socialOnly: { type:Boolean, default: false }, // github로 로그인 했는지 check
-    username: { type: String, required: true, unique: true },
-    password: { type: String },
-    name: { type: String, required:true },
-    location: String,
-    videos: [{ type: mongoose.Schema.Types.ObjectId, ref: "Video" }],
-    comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
+  email: { type: String, required: true, unique: true },
+  avatarUrl: String,
+  socialOnly: { type: Boolean, default: false }, // github로 로그인 했는지 check
+  username: { type: String, required: true, unique: true },
+  password: { type: String },
+  name: { type: String, required: true },
+  location: String,
+  videos: [{ type: mongoose.Schema.Types.ObjectId, ref: "Video" }],
+  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
 });
 
-userSchema.pre('save', async function() {
-    if(this.isModified("password")){
-        this.password = await bcrypt.hash(this.password, 5);
-    };
+userSchema.pre("save", async function () {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 5);
+  }
 });
 
 const User = mongoose.model("User", userSchema);
