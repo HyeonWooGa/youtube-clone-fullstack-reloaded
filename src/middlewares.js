4d7d5c2d34f56aeed3cf9649ae2bba66a9807bee
HyeonWooGa <yeonwoopark22@gmail.com>
2022-06-10 09:22:42 +0900

    s3 uploads

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,39 +1,56 @@
 import multer from "multer";
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
+
+const s3 = new aws.S3({
+  credentials: {
+    accessKeyId: process.env.AWS_ID,
+    secretAccessKey: process.env.AWS_SECRET,
+  },
+});
+
+const multerUploader = multerS3({
+  s3: s3,
+  bucket: "wootubee",
+  acl: "publick-read",
+});
 
 export const localsMiddleware = (req, res, next) => {
-    res.locals.loggedIn = Boolean(req.session.loggedIn); // req.session.loggedIn 이 null, undefined 일 수 있으니 Boolean 으로 감싸줌
-    res.locals.siteName = "Wetube";
-    res.locals.loggedInUser = req.session.user || {}; // req.session.user 가 undefined 일 수 있으니 logeedInUser 는 loggedIn 상태에서만 사용 ex)if loggedIn 하위 항목으로
-    next();
+  res.locals.loggedIn = Boolean(req.session.loggedIn); // req.session.loggedIn 이 null, undefined 일 수 있으니 Boolean 으로 감싸줌
+  res.locals.siteName = "Wetube";
+  res.locals.loggedInUser = req.session.user || {}; // req.session.user 가 undefined 일 수 있으니 logeedInUser 는 loggedIn 상태에서만 사용 ex)if loggedIn 하위 항목으로
+  next();
 };
 
 export const protectorMiddleware = (req, res, next) => {
-    if(req.session.loggedIn) {
-        return next();
-    } else {
-        req.flash("error", "Log in first");
-        return res.redirect("/login");
-    }
+  if (req.session.loggedIn) {
+    return next();
+  } else {
+    req.flash("error", "Log in first");
+    return res.redirect("/login");
+  }
 };
 
 export const publicOnlyMiddleware = (req, res, next) => {
-    if(!req.session.loggedIn) {
-        return next();
-    } else {
-        req.flash("error", "Not authorized");
-        return res.redirect("/");
-    }
+  if (!req.session.loggedIn) {
+    return next();
+  } else {
+    req.flash("error", "Not authorized");
+    return res.redirect("/");
+  }
 };
 
-export const avatarUpload = multer({ 
-    dest: "uploads/avatars/", 
-    limits: {
-        fileSize: 3000000,
-    }  
+export const avatarUpload = multer({
+  dest: "uploads/avatars/",
+  limits: {
+    fileSize: 3000000,
+  },
+  storage: multerUploader,
 });
-export const videoUpload = multer({ 
-    dest: "uploads/videos/", 
-    limits: {
-        fieldSize: 10000000,
-    } 
+export const videoUpload = multer({
+  dest: "uploads/videos/",
+  limits: {
+    fieldSize: 10000000,
+  },
+  storage: multerUploader,
 });
